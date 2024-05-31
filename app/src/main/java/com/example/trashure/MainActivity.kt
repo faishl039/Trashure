eb5b1ffda1a55fac89e8d6ecf298b29c90e6d393
Faishal Akbar S <faishalakbarsyahputra@gmail.com>
2024-05-31 09:36:43 +0700

    adding fragment bank, upload, news, profile

--- app/src/main/java/com/example/trashure/MainActivity.kt
+++ app/src/main/java/com/example/trashure/MainActivity.kt
@@ -26,7 +26,11 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_bank_sampah,
+                R.id.navigation_upload,
+                R.id.navigation_news,
+                R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
