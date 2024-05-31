70ffc5a09b09e14f43ebf913c5f4350f60050953
Faishal Akbar S <faishalakbarsyahputra@gmail.com>
2024-05-31 10:15:37 +0700

    adding webview implementation

--- app/src/main/java/com/example/trashure/ui/banksampah/BankSampahFragment.kt
+++ app/src/main/java/com/example/trashure/ui/banksampah/BankSampahFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.example.trashure.R
+import com.example.trashure.databinding.FragmentBankSampahBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,24 +22,34 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BankSampahFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentBankSampahBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bank_sampah, container, false)
+        _binding = FragmentBankSampahBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val webview: WebView = binding.webview
+        webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return true
+            }
+        }
+        webview.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            cacheMode = android.webkit.WebSettings.LOAD_NO_CACHE
+        }
+        webview.loadUrl("https://waste4change.com/sendyourwaste")
+
+        return root
     }
 
     companion object {
