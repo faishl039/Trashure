e11d7d2bceb073e1eb276fc3c56acb8e76e7e931
Faishal Akbar S <faishalakbarsyahputra@gmail.com>
2024-06-11 15:04:10 +0700

    adding response from json

---
+++ app/src/main/java/com/example/trashure/network/response/TrashureRegisterResponse.kt
@@ -0,0 +1,9 @@
+package com.example.trashure.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class TrashureRegisterResponse(
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
