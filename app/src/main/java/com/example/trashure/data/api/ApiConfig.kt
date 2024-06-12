9c9362bb355b73fed55c3a8f320113239b840d60
Faishal Akbar S <faishalakbarsyahputra@gmail.com>
2024-06-13 00:21:47 +0700

    making to companion object

--- app/src/main/java/com/example/trashure/data/api/ApiConfig.kt
+++ app/src/main/java/com/example/trashure/data/api/ApiConfig.kt
@@ -8,34 +8,32 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
-    fun getApiService(token: String?): ApiService {
-        val loggingInterceptor =
-            if (BuildConfig.DEBUG) {
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            } else {
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-            }
-        val authInterceptor = Interceptor { chain ->
-            val req = chain.request()
-            val requestHeaders = req.newBuilder()
-            if (!token.isNullOrEmpty()) {
-                requestHeaders.addHeader("Authorization", "Bearer $token")
-            }
-            chain.proceed(requestHeaders.build())
-        }
-        val client = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .addInterceptor(authInterceptor)
-            .build()
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-        return retrofit.create(ApiService::class.java)
-    }
-
     companion object {
-        const val BASE_URL = "https://backend-dot-vernal-house-425717-g2.dt.r.appspot.com/api/"
+        fun getApiService(token: String?): ApiService {
+            val loggingInterceptor =
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+                } else {
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+                }
+            val authInterceptor = Interceptor { chain ->
+                val req = chain.request()
+                val requestHeaders = req.newBuilder()
+                if (!token.isNullOrEmpty()) {
+                    requestHeaders.addHeader("x-access-token", "$token")
+                }
+                chain.proceed(requestHeaders.build())
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .addInterceptor(authInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://backend-dot-vernal-house-425717-g2.dt.r.appspot.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
     }
 }
