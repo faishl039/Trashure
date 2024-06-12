f5a027058aec6eefce2cb52d0f38b564cb415362
Faishal Akbar S <faishalakbarsyahputra@gmail.com>
2024-06-13 00:09:05 +0700

    fixing function post image with token

--- app/src/main/java/com/example/trashure/ui/upload/UploadFragment.kt
+++ app/src/main/java/com/example/trashure/ui/upload/UploadFragment.kt
@@ -12,6 +12,8 @@ import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.trashure.ViewModelFactory
 import com.example.trashure.databinding.FragmentUploadBinding
 import com.example.trashure.util.getImageUri
 import com.example.trashure.util.reduceFileImage
@@ -19,9 +21,12 @@ import com.example.trashure.util.uriToFile
 
 class UploadFragment : Fragment() {
     private var _binding: FragmentUploadBinding? = null
-    private val viewModel: UploadVM by viewModels()
+    private val viewModel by viewModels<UploadVM> {
+        ViewModelFactory.getInstance(requireContext())
+    }
     private val binding get() = _binding!!
     private var currentImageUri: Uri? = null
+    private var token: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +34,19 @@ class UploadFragment : Fragment() {
     ): View {
         _binding = FragmentUploadBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        viewModel.getSession().observe(viewLifecycleOwner) {
+            token = it.email
+            binding.uploadButton.setOnClickListener {
+                token?.let {
+                    uploadTrash(it)
+                }
+            }
+            val tokenMessage = "Token: ${token}"
+            makeToast(tokenMessage)
+            Log.d("upload", tokenMessage)
+        }
+
 
         obsUploadTrash()
         obsLoading()
@@ -39,24 +57,25 @@ class UploadFragment : Fragment() {
         binding.galleryButton.setOnClickListener {
             openGallery()
         }
-        binding.uploadButton.setOnClickListener {
-            uploadTrash()
-        }
 
         return root
     }
 
     private fun obsLoading() {
-        viewModel.isLoading.observe(viewLifecycleOwner) {
+        viewModel.isLoad.observe(viewLifecycleOwner) {
             showLoading(it)
         }
     }
 
     private fun obsUploadTrash() {
-        viewModel.uploadTrash.observe(viewLifecycleOwner) {
+        viewModel.upImageResp.observe(viewLifecycleOwner) {
             if (it != null) {
                 Log.d(TAG, "upload successful: ${it.message}")
                 makeToast("upload successful: ${it.message}")
+                moveToResult(currentImageUri!!)
+//                currentImageUri?.let {
+//                    moveToResult(it)
+//                }
             } else {
                 Log.e(TAG, "upload failed: No response from server")
             }
@@ -97,17 +116,19 @@ class UploadFragment : Fragment() {
         }
     }
 
-    private fun uploadTrash() {
+    private fun uploadTrash(token: String) {
         currentImageUri?.let {
             val imageFile = uriToFile(it, requireContext()).reduceFileImage()
             Log.d(TAG, "show path image: ${imageFile.path}")
-            viewModel.postImageTrash(imageFile)
-        }?: makeToast("foto tidak ada")
+            viewModel.postImageTrash(token, imageFile)
+        } ?: makeToast("foto tidak ada")
     }
 
-//    private fun moveToResult() {
-//        val intent = Intent(requireContext(), UploadSuccessFragment)
-//    }
+    private fun moveToResult(imageUri: Uri) {
+        val intent = Intent(requireContext(), ResultActivity::class.java)
+        intent.putExtra("image", imageUri.toString())
+        startActivity(intent)
+    }
 
     private fun makeToast(message: String) {
         Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
